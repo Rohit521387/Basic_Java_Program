a57c6764df58d356c7608b4a8d8d421dc107e443
Rohit Kadam <kadamr574@gmail.com>
2024-08-05 01:08:21 +0530

    Pushing my basic java program code

---
+++ Addition.java
@@ -0,0 +1,17 @@
+package basics.addition;
+
+public class Addition
+{
+
+	public static void main(String[] args) 
+	{
+	
+	        System.out.println("INDIA");
+	        System.out.println("World");
+	
+	}
+	
+	
+	
+	
+}
