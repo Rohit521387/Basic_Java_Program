8da00370bf5d074426b3ed0c2110be8902a7f2a9
Rohit Kadam <Kadamr574@gmail.com>
2024-08-05 20:50:43 +0530

    Basic java different class program in the same package

---
+++ Student.java
@@ -0,0 +1,15 @@
+package java_Package;
+
+public class Student 
+{
+
+	public static void main(String[] args) 
+	{
+		Teacher t1=new Teacher();
+		t1.add();
+		t1.sub();
+		t1.div();
+		
+	}
+
+}
---
+++ Teacher.java
@@ -0,0 +1,36 @@
+package java_Package;
+
+public class Teacher 
+{
+	public void add()
+	   {
+	    	int a=50;
+	    	int b=50;
+	    	int sum=a+b;
+	    	System.out.println("Addition of 2 Numbers is "+sum );
+	    	
+	   }
+	   protected void sub()
+	   {
+		   int a=100;
+	   	   int b=50;
+	   	   int sub=a-b;
+	   	   System.out.println("Subtraction of 2 Numbers is "+sub );
+	   }
+	   private void mul()
+	   {
+		   int a=50;
+	   	   int b=50;
+	   	   int mul=a*b;
+	   	   System.out.println("Multiplication of 2 Numbers is "+mul );
+	   }
+	   void div()
+	   {
+		   int a=50;
+	   	   int b=10;
+	   	   int div=a/b;
+	   	   System.out.println("Division of 2 Numbers is "+div );
+	   }
+	
+
+}
